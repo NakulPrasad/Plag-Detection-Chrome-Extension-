654af070621cbf73ff78278ea1e09a4728f2bf64
Nakul Prasad <nakulprasad10@outlook.com>
2024-04-16 23:35:46 +0530

    added submission date to be shown

--- background/background.js
+++ background/background.js
@@ -2,16 +2,22 @@ let apiData;
 
 (async () => {
     try {
-        const res = await fetch("https://mentorpick.com/api/courseV2/contest/submission/my?problem=&verdictString=ACCEPTED&contestSlug=bz-bvrith-y22-phase-1-week-1-practice&language=&limit=100&page=1&user=jangili&courseId=65fadb136edf77d59a861c05&contestId=5384ef75-30ae-4101-bfd8-7a7645869000");
+        const res = await fetch("https://mentorpick.com/api/courseV2/contest/submission/my?problem=&verdictString=ACCEPTED&contestSlug=bz-bvrith-y22-phase-1-week-1-practice&language=&limit=100&page=1&user=23wh5a0515-jangili&courseId=65fadb136edf77d59a861c05&contestId=5384ef75-30ae-4101-bfd8-7a7645869000");
 
         if (!res.ok) {
             throw new Error('Failed to get api');
         }
         apiData = await res.json();
 
+        // console.log(apiData.data);
+
+        const uniqueSubmissions = removeDuplicates(apiData.data, "problem");
+
+        // console.log(uniqueSubmissions);
+
         //sorting the object array
-        if (apiData) {
-            apiData.data.sort((a, b) => {
+        if (uniqueSubmissions) {
+            uniqueSubmissions.sort((a, b) => {
                 const timeA = Date.parse(a.created_at);
                 const timeB = Date.parse(b.created_at);
                 return timeA - timeB;
@@ -33,7 +39,7 @@ let apiData;
                 console.log("Received submissionCount:", submissionCount);
                 console.log("Received timeDifference:", timeDifference);
 
-                const checkPlag = detectPlagiarism(apiData.data, timeDifference, submissionCount);
+                const checkPlag = detectPlagiarism(uniqueSubmissions, timeDifference, submissionCount);
                 console.log(checkPlag);
                 if (checkPlag) {
                     chrome.runtime.sendMessage({ "verdict": checkPlag });
@@ -69,6 +75,7 @@ function detectPlagiarism(submissions, timeDifference, submissionCount) {
 
         // Check if submissions in window exceed the threshold
         if (submissionsInWindow > submissionCount) {
+            printSubmission(startIndex, i, submissionTimes);
             return 'true';
         }
     }
@@ -76,6 +83,41 @@ function detectPlagiarism(submissions, timeDifference, submissionCount) {
     return 'false';
 }
 
+function printSubmission(startIndex, endIndex, submissionTimes) {
+    const plagiarismIndices = [];
+    for (let j = startIndex; j <= endIndex; j++) {
+        plagiarismIndices.push(submissionTimes[j]);
+    }
+    // const dateObjects = plagiarismIndices.map(timestamp => new Date(timestamp));
+    const dateObjects = plagiarismIndices.map(timestamp => {
+
+        const date = new Date(timestamp);
+        return date.toLocaleString('en-US', {
+            timeZone: 'Asia/Kolkata', hour: 'numeric', minute: 'numeric', hour12: true, day: 'numeric',
+            month: 'numeric',
+            year: 'numeric'
+        });
+    });
+
+    console.log(dateObjects);
+    // const arr = JSON.stringify(plagiarismIndices);
+    // console.log(arr);
+    // plagiarismIndices.forEach(submisssion =>{
+    //     console.log(submisssion);
+    // })
+}
+
+function removeDuplicates(submissions, field) {
+    const uniqueSubmissions = submissions.reduce((acc, curr) => {
+        const fieldValue = curr[field].title;
+        if (!acc.has(fieldValue)) {
+            acc.set(fieldValue, curr);
+        }
+        return acc;
+    }, new Map());
+
+    return Array.from(uniqueSubmissions.values());
+}
 
 
 
