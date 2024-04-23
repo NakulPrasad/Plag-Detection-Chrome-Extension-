bd9d7736b7b4e1bdc1a7dc134e1fe8b11c730961
Nakul Prasad <nakulprasad10@outlook.com>
2024-04-23 16:50:20 +0530

    minor changes

--- popup/floatingButton.js
+++ popup/floatingButton.js
@@ -1,6 +1,6 @@
 function checkUrlAndCallApi() {
     const currentUrl = window.location.href;
-    
+
     if (currentUrl.startsWith('https://mentorpick.com/profile/')) {
         const profileId = currentUrl.split('profile/')[1];
         insertButton();
@@ -9,21 +9,32 @@ function checkUrlAndCallApi() {
 }
 
 checkUrlAndCallApi();
-            
-function insertButton(){
+
+function insertButton() {
     console.log("hi");
-    const checkInterval = setInterval(()=>{
+    const checkInterval = setInterval(() => {
         const header = document.querySelectorAll('#root > div:nth-child(2) > div > div > div.mantine-Col-root.mantine-cpanvy > div > div.mantine-Paper-root.mantine-1gi5o8w')[0];
-        if(header) 
-        {
+        if (header) {
             clearInterval(checkInterval)
-            console.log(header);
             let newButton = document.createElement('button');
+            newButton.id = "checkPlag";
             newButton.textContent = "Check Plag";
             newButton.style.backgroundColor = "Red";
+            newButton.style.cursor = 'pointer'
             header.appendChild(newButton);
-        
+
+            const modal = new bootstrap.Modal(document.getElementById("modal"))
+
+            newButton.addEventListener('click', () => {
+                handleClick();
+            })
+
         }
-    },5000);
-    
+    }, 5000);
+
+}
+
+function handleClick(){
+    console.log('show modal');
+
 }
